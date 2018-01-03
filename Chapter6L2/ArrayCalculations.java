6f402d245d2b821fbb7baafdc6fbaf865d9a2fb9
anicazulch <31353283+anicazulch@users.noreply.github.com>
2018-01-03 09:53:55 -0800

    Chapter 6 Commit

---
+++ Chapter6L2/ArrayCalculations.java
@@ -0,0 +1,35 @@
+public class ArrayCalculations {
+    public ArrayCalculations(){
+    }
+    public double averageArray(int[][] nums){
+        int sum = 0;
+        for (int row = 0; row < nums.length; row++) {
+            for (int col = 0; col < nums[0].length; col++){
+                sum+= nums[row][col]; // I want to get to that row and col and add that value
+            }
+        }
+        int total = nums.length * nums[0].length; // of elements to know what to divide by
+        return (double) sum/total;
+    }
+    public int[] sumColumns(int[][] nums){
+        int[] result = new int[nums[0].length];
+        for (int col = 0; col < nums[0].length; col++){
+            int total = 0;
+            for (int row = 0; row < nums.length; row++){
+                total += nums[row][col];
+            }
+            result[col] = total;
+        }
+        return result;
+    }    
+    public int[][] squareArray(int[][] nums){
+        int[][] result = new int[nums.length][nums[0].length]; // have made array with all 0s not tied to nums won't screw up
+        for (int row = 0; row < nums.length; row++){
+            for (int col = 0; col < nums[0].length; col++){
+                int value = nums[row][col];
+                result[row][col] = (int) Math.pow(value,2); // or just value * value
+            }
+        }
+        return result;
+    }
+}
---
+++ Chapter6L2/ArrayTester.java
@@ -0,0 +1,25 @@
+public class ArrayTester{
+    public static void main(String[] args){
+        int[][] arr1 = {{1,1,1},
+                       {1,1,1}};
+        int[][] arr2 = {{3,2,2},
+                       {4,2,1}};
+        ArrayCalculations ac = new ArrayCalculations();
+        double avg = ac.averageArray(arr1);
+        System.out.println("The average is " + avg);
+        
+        int[] result = ac.sumColumns(arr1);
+        for (int thing: result) {
+            System.out.print(thing + ", ");
+        }
+        
+        int[][] squared = ac.squareArray(arr2);
+        for (int row : squared){
+            for (int num : row) {
+                System.out.print(num + ", ");
+            }
+            System.out.println();
+        }
+        
+    }
+    }
